c11f95ef49bacfcdd104305b679c7419c9c8ec65
Andrew Landsverk <dewdew@gmail.com>
2023-06-27 22:32:16 -0500

    initial commit of blank soapbox application

---
+++ src/main/java/com/quantasnet/soapbox/SoapboxApiApplication.java
@@ -0,0 +1,13 @@
+package com.quantasnet.soapbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapboxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapboxApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantasnet/soapbox/SoapboxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantasnet.soapbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapboxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
